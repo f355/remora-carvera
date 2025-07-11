ae5817ddd24912531454824f5653b395d37d7da5
Konstantin Tcepliaev <f355@f355.org>
2025-07-11 15:20:06 +0200

    backport stepgen logic and LinuxCNC HAL from flexi-nc

--- Firmware/FirmwareSource/Remora-OS5/modules/stepgen/stepgen.cpp
+++ Firmware/FirmwareSource/Remora-OS5/modules/stepgen/stepgen.cpp
@@ -8,10 +8,9 @@
 void createStepgen()
 {
     const char* comment = module["Comment"];
-    printf("%s\n",comment);
+    printf("\n%s\n",comment);
 
     int joint = module["Joint Number"];
-    const char* enable = module["Enable Pin"];
     const char* step = module["Step Pin"];
     const char* dir = module["Direction Pin"];
 
@@ -21,8 +20,9 @@ void createStepgen()
     ptrJointEnable = &rxData.jointEnable;
 
     // create the step generator, register it in the thread
-    Module* stepgen = new Stepgen(base_freq, joint, enable, step, dir, STEPBIT, *ptrJointFreqCmd[joint], *ptrJointFeedback[joint], *ptrJointEnable);
+    Module* stepgen = new Stepgen(base_freq, joint, step, dir, STEPBIT, *ptrJointFreqCmd[joint], *ptrJointFeedback[joint], *ptrJointEnable);
     baseThread->registerModule(stepgen);
+    baseThread->registerModulePost(stepgen);
 }
 
 
@@ -30,9 +30,8 @@ void createStepgen()
                 METHOD DEFINITIONS
 ************************************************************************/
 
-Stepgen::Stepgen(int32_t threadFreq, int jointNumber, std::string enable, std::string step, std::string direction, int stepBit, volatile int32_t &ptrFrequencyCommand, volatile int32_t &ptrFeedback, volatile uint8_t &ptrJointEnable) :
+Stepgen::Stepgen(int32_t threadFreq, int jointNumber, std::string step, std::string direction, int stepBit, volatile int32_t &ptrFrequencyCommand, volatile int32_t &ptrFeedback, volatile uint8_t &ptrJointEnable) :
 	jointNumber(jointNumber),
-	enable(enable),
 	step(step),
 	direction(direction),
 	stepBit(stepBit),
@@ -40,14 +39,15 @@ Stepgen::Stepgen(int32_t threadFreq, int jointNumber, std::string enable, std::s
 	ptrFeedback(&ptrFeedback),
 	ptrJointEnable(&ptrJointEnable)
 {
-	this->enablePin = new Pin(this->enable, OUTPUT);			// create Pins
 	this->stepPin = new Pin(this->step, OUTPUT);
 	this->directionPin = new Pin(this->direction, OUTPUT);
 	this->DDSaccumulator = 0;
+    this->rawCount = 0;
 	this->frequencyScale = (float)(1 << this->stepBit) / (float)threadFreq;
 	this->mask = 1 << this->jointNumber;
 	this->isEnabled = false;
 	this->isForward = false;
+    this->lastDir = !this->isForward;
 }
 
 
@@ -55,6 +55,11 @@ void Stepgen::update()
 {
 	// Use the standard Module interface to run makePulses()
 	this->makePulses();
+}
+
+void Stepgen::updatePost()
+{
+	this->stopPulses();
 }
 
 void Stepgen::slowUpdate()
@@ -70,15 +75,13 @@ void Stepgen::makePulses()
 
 	if (this->isEnabled == true)  												// this Step generator is enables so make the pulses
 	{
-		this->enablePin->set(false);                                			// Enable the driver - CHANGE THIS TO MAKE THE OUTPUT VALUE CONFIGURABLE???
-
 		this->frequencyCommand = *(this->ptrFrequencyCommand);            		// Get the latest frequency command via pointer to the data source
 		this->DDSaddValue = this->frequencyCommand * this->frequencyScale;		// Scale the frequency command to get the DDS add value
 		stepNow = this->DDSaccumulator;                           				// Save the current DDS accumulator value
 		this->DDSaccumulator += this->DDSaddValue;           	  				// Update the DDS accumulator with the new add value
 		stepNow ^= this->DDSaccumulator;                          				// Test for changes in the low half of the DDS accumulator
 		stepNow &= (1L << this->stepBit);                         				// Check for the step bit
-		this->rawCount = this->DDSaccumulator >> this->stepBit;   				// Update the position raw count
+		//this->rawCount = this->DDSaccumulator >> this->stepBit;   				// Update the position raw count
 
 		if (this->DDSaddValue > 0)												// The sign of the DDS add value indicates the desired direction
 		{
@@ -89,24 +92,39 @@ void Stepgen::makePulses()
 			this->isForward = false;
 		}
 
-		if (stepNow)
+        if (this->lastDir != this->isForward)
+        {
+            //Direction has changed, flip dir pin and do not step this iteration to give some setup time. At a 160kHz base thread freq, this should be about 6.25us, at 120kHz 8.33us (1 period). JMC servos requre 6us. TODO - make hold time configurable.
+            this->lastDir = this->isForward;
+            this->directionPin->set(this->isForward);             		// Set direction pin
+        }
+		else if (stepNow)
 		{
-			this->directionPin->set(this->isForward);             		// Set direction pin
-			this->stepPin->set(true);										// Raise step pin - A4988 / DRV8825 stepper drivers only need 200ns setup time
-			*(this->ptrFeedback) = this->DDSaccumulator;                     // Update position feedback via pointer to the data receiver
-		}
-		else
-		{
-			this->stepPin->set(false);										// Reset step pin
-		}
+            this->stepPin->set(true);										// Raise step pin - A4988 / DRV8825 stepper drivers only need 200ns setup time
+            if (this->isForward)
+            {
+                this->rawCount++;
+            }
+            else
+            {
+                this->rawCount--;
+            }
+            *(this->ptrFeedback) = this->rawCount;							// Update position feedback via pointer to the data receiver
+			this->isStepping = true;
 
+		}
 	}
-	else
-	{
-		this->enablePin->set(true);
-	}
+
 
 }
+
+
+void Stepgen::stopPulses()
+{
+	this->stepPin->set(false);	// Reset step pin
+	this->isStepping = false;
+}
+
 
 void Stepgen::setEnabled(bool state)
 {
